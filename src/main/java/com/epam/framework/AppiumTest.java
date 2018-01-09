8e69b569f65185580aa6af13d6de04b2fcfe4b89
Aibar-B <aibro_85@mail.ru>
2018-01-09 17:11:00 +0600

    add assertion

--- src/main/java/com/epam/framework/AppiumTest.java
+++ src/main/java/com/epam/framework/AppiumTest.java
@@ -7,6 +7,7 @@ import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -31,7 +32,7 @@ public class AppiumTest {
         driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
     }
 
-    @Test(dataProvider = "dataForLetter" , dataProviderClass = DataProviderClass.class)
+    @Test(dataProvider = "dataForLetter", dataProviderClass = DataProviderClass.class)
     public void makeNote(Letter letter) {
         driver.findElement(By.className("android.widget.ImageButton")).click();
         List<AndroidElement> list = driver.findElements(By.className("android.widget.EditText"));
@@ -39,12 +40,14 @@ public class AppiumTest {
             if (androidElement.getText().equals("Add title")) androidElement.sendKeys(letter.getSubject());
             if (androidElement.getText().equals("Add content")) androidElement.sendKeys(letter.getTextContent());
         }
-        driver.findElements(By.className("android.widget.TextView")).get(0).click();
+        
+        driver.findElement(By.id("action_save")).click();
+        Assert.assertEquals(driver.findElement(By.id("note_content")).getText(), letter.getTextContent());
     }
 
     @AfterTest
-    public void closeBrowser(){
+    public void closeBrowser() {
         driver.quit();
     }
-
 }
+
